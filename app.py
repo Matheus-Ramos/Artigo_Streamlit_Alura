f49ed4a7cae5f5b63cb4e12cff763a6f3569d83a
Matheus Ramos <matheusr808@gmail.com>
2025-05-09 18:42:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def plot_estoque(dataframe, categoria):
     return fig
 
 # importando os dados
-dados = pd.read_csv('dados/estoque.csv')
+dados = pd.read_csv('estoque.csv')
 
 st.title('Análise de estoque\n')
 st.write('Nesse projeto vamos analisar a quantidade de produtos em estoque, por categoria, de uma base de dados de produtos de supermercado')
